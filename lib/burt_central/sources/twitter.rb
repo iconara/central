f9c1e5ae17b5e4028f04c8c6e50e56a0c0cdcf4a
Theo <theo@iconara.net>
2010-03-17 08:40:08 +0100

    Rewrote the Twitter source so that it loads all tweets since the specified date

--- lib/burt_central/sources/twitter.rb
+++ lib/burt_central/sources/twitter.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'twitter'
 
 
@@ -9,16 +10,32 @@ module BurtCentral
       def events(since)
         logger.info('Loading tweets')
         
-        tweets = ::Twitter.list_timeline('burtcorp', 'meet-the-burts')
-        tweets.map do |tweet|
-          Event.new(
-            :title => tweet.text,
-            :date => Time.parse(tweet.created_at),
-            :instigator => tweet.user.name,
-            :url => "http://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}",
-            :type => :tweet
-          )
+        events = []
+        page = 1
+        
+        catch(:all_found) do
+          loop do
+            logger.debug("Loading page #{page}")
+          
+            tweets = ::Twitter.list_timeline('burtcorp', 'meet-the-burts', :page => page)
+        
+            tweets.each do |tweet|
+              throw :all_found unless Date.parse(tweet.created_at) >= since
+              
+              events << Event.new(
+                :title => tweet.text,
+                :date => Time.parse(tweet.created_at),
+                :instigator => tweet.user.name,
+                :url => "http://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}",
+                :type => :tweet
+              )
+            end
+        
+            page += 1
+          end
         end
+        
+        events
       end
     end
   end
