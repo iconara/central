f0a2cbe590764f44770ac29b1d44988977648c77
Theo <theo@iconara.net>
2010-03-25 19:20:16 +0100

    Wrapped the app.rb code in a Sinatra::Base subclass

--- app.rb
+++ app.rb
@@ -1,41 +1,49 @@
-$: << File.expand_path('../lib', __FILE__)
-
+require 'sinatra'
 require 'mongo'
 require 'json'
 require 'less'
 require 'burt_central'
 
 
-configure do
-  configuration = BurtCentral::Configuration.load(File.expand_path('../config/common.yml', __FILE__))
-  
-  DATABASE = Mongo::Connection.new.db('burt_central')
-  EVENTS_COLLECTION = DATABASE.collection('events')
-end
+class App < Sinatra::Base
 
-before do
-  content_type :json
-end
+  set :environment, ENV['RACK_ENV'].to_sym
+  set :root,        File.dirname(__FILE__)
 
-get '/history' do
-  options = {}
+  disable :run
   
-  if params[:limit] && params[:limit].to_i > 0
-    options[:limit] = params[:limit].to_i
-  else
-    options[:since] = Time.now - (24 * 60 * 60)
+  configure do
+    configuration = BurtCentral::Configuration.load(File.expand_path('../config/common.yml', __FILE__))
+  
+    DATABASE = Mongo::Connection.new.db('burt_central')
+    EVENTS_COLLECTION = DATABASE.collection('events')
   end
+
+  before do
+    content_type :json
+  end
+
+  get '/history' do
+    options = {}
   
-  history = BurtCentral::History.new
-  history.restore(EVENTS_COLLECTION, options)
-  history.events.map { |e| e.to_h }.to_json
-end
+    if params[:limit] && params[:limit].to_i > 0
+      options[:limit] = params[:limit].to_i
+    else
+      options[:since] = Time.now - (24 * 60 * 60)
+    end
+  
+    history = BurtCentral::History.new
+    history.restore(EVENTS_COLLECTION, options)
+    history.events.map { |e| e.to_h }.to_json
+  end
 
-get '/types' do
-  EVENTS_COLLECTION.distinct(:type).to_json
-end
+  get '/types' do
+    EVENTS_COLLECTION.distinct(:type).to_json
+  end
 
-get '/styles/app.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  less :app
+  get '/styles/app.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    less :app
+  end
+
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
+$: << File.expand_path('../lib', __FILE__)
+
 begin
   require File.expand_path('../.bundle/environment', __FILE__)
 rescue LoadError
@@ -7,17 +9,6 @@ rescue LoadError
   Bundler.setup
 end
 
-require 'sinatra'
-
-
-root_dir = File.dirname(__FILE__)
-
-set :environment, ENV['RACK_ENV'].to_sym
-set :root,        root_dir
-set :app_file,    File.join(root_dir, 'app.rb')
-
-disable :run
-
 require 'app'
 
-run Sinatra::Application
+run App
